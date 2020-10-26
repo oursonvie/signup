e1b3f35cb1e54c155bd8915f6fbe1fe3d643b10b
Tonny <oursonvie@qq.com>
2020-10-26 08:52:44 +0800

    update

--- client/studentInfo/updateStudentForm.js
+++ client/studentInfo/updateStudentForm.js
@@ -14,12 +14,18 @@ Template.updateStudentForm.events({
     let first_name = document.getElementById('first_name').value
     let language = document.getElementById('language').value
 
+    // check input string
+
+    let regex = /^[A-Za-z]+$/
+
     if ( !family_name ) {
       alert(`姓氏不能为空`)
     } else if (!first_name) {
       alert(`名字不能为空`)
     } else if ( language == '选择' ) {
       alert(`请选择报考语言`)
+    } else if ( !regex.test(family_name) || !regex.test(first_name) ) {
+      alert(`请输入姓名拼音`)
     } else {
       PromiseMeteorCall('signupStudent', this._id, family_name, first_name, language)
       .then( res => {
