dce3b6801c24b53ee532bd55f50ddff20742a91f
Tonny <oursonvie@qq.com>
2020-10-28 10:31:01 +0800

    update

---
+++ server/methods/pinyin.js
@@ -0,0 +1,26 @@
+var pinyin = require("chinese-to-pinyin")
+
+let regex = /^[A-Za-z]+$/
+
+checkPinyin = () => {
+  let list = Student.find({edited:true},{fields:{family_name:1, first_name:1}}).fetch()
+
+  _.forEach(list, function(student) {
+
+    // check family name
+    family_name = ( regex.test(student.family_name) ) ? student.family_name : capitalizeFirstLetter(pinyin( student.family_name, {removeTone: true, removeSpace: true} ))
+
+    // check first name
+    first_name = ( regex.test(student.first_name) ) ? student.first_name : capitalizeFirstLetter(pinyin(student.first_name, {removeTone: true, removeSpace: true}))
+
+    result = Student.update( {_id:student._id}, {$set:{family_name:family_name, first_name:first_name}} )
+
+    console.log(`[checkPinyin], updating ${student._id}`)
+
+  })
+
+}
+
+capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- server/methods/test.js
+++
@@ -1,6 +0,0 @@
-var pinyin = require("chinese-to-pinyin")
-
-
-testtest = () => {
-  console.log(pinyin('今天天气真好', {removeTone: true}))
-}
