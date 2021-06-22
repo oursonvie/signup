5e1ad035436861c6521d2002d3f956ff74991868
Tonny <oursonvie@qq.com>
2021-06-22 14:42:24 +0800

    update

--- server/methods/downloadSeatings.js
+++ server/methods/downloadSeatings.js
@@ -5,19 +5,19 @@ Meteor.methods({
     console.log('[downloadSeats] cleaning SeatDownload')
     SeatDownload.remove({})
 
-    seats = Seats.find({},{fields:{_id:0, studentid:1, roomnumber:1, lcenter:1, seatnumber: 1, name: 1, certno: 1, examid: 1, source: 1, language: 1, examTime:1}}).fetch()
+    let seats = Seats.find({},{fields:{_id:0, studentid:1, roomnumber:1, lcenter:1, seatnumber: 1, name: 1, certno: 1, examid: 1, source: 1, language: 1, examTime:1}}).fetch()
 
     // insert eversingle one student with room name into db
 
     _.forEach(seats, function(seat) {
 
-      seatInfo = seat
+      let seatInfo = seat
 
-      examRoomInfo = Examroom.findOne({examroomId:seat.roomnumber},{fields:{address:1, examroomLocation:1, place:1, _id:0}})
+      let examRoomInfo = Examroom.findOne({examroomId:seat.roomnumber},{fields:{address:1, examroomLocation:1, place:1, _id:0}})
 
       let studentInfo = Student.findOne({certno: seat.certno}, {fields:{family_name:1, first_name:1, _id:0}})
 
-      combined = {...seatInfo, ...examRoomInfo, ...studentInfo}
+      let combined = {...seatInfo, ...examRoomInfo, ...studentInfo}
 
       SeatDownload.insert(combined)
 
