feb14ce699ae952562f9d4ea5d55b40e499785e3
Tonny <oursonvie@qq.com>
2021-06-28 08:38:38 +0800

    update facelog sort

--- client/admin/facelog/facelog.js
+++ client/admin/facelog/facelog.js
@@ -8,7 +8,7 @@ Template.facelog.onCreated(function() {
 
 Template.facelog.helpers({
   Facelogs: function() {
-    return FaceLogs.find()
+    return FaceLogs.find({}, {sort:{createdAt:-1}})
   },
   dateTime: function() {
     return moment(this.createdAt).format('YYYY-MM-DD HH:mm:ss')
